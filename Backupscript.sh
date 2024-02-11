7684079f707803c4c17be02c311f943031a7cac9
abishekanand <abishekanand15@gmail.com>
2024-02-11 13:23:49 +0530

    backupsript added

--- Backupscript.sh
+++ Backupscript.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 
-# Set variables
-BACKUP_DIR="/home/ec2-user/BashScripting-Backup"
-SOURCE_DIR="/home/ec2-user/BashScripting/"
-
-# Create backup directory if not exists...
-mkdir -p $BACKUP_DIR
-
-# Backup files
-echo "Backing up files from $SOURCE_DIR to $BACKUP_DIR..."
-cp -r $SOURCE_DIR $BACKUP_DIR
-
-echo "Backup completed successfully..!"
