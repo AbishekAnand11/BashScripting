21f497efa46c7c7959c8e1dd4e12e2c20a808385
abishekanand <abishekanand15@gmail.com>
2024-02-14 12:06:23 +0530

    File process and count lines

--- IterateFiles.sh
+++ IterateFiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 for file in *.txt; do
+#Check whether the files exists and its a proper file? ( -f )
   if [ -f $file ]; then
+    #wc -l = counts the number of lines
     filecount=$(wc -l < $file)
     echo "$file : $filecount"
   fi  
