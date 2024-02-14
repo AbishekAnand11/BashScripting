e6ac1ffe05780c4b5a16c8d4a01b00e8c3cab7ec
abishekanand <abishekanand15@gmail.com>
2024-02-14 11:59:58 +0530

    Sample file edited

--- Sample.sh
+++ Sample.sh
@@ -1,15 +1,1 @@
 #!/bin/bash
-mkdir logs
-cd logs
-logfile = "error_log.txt"
-if [ ! -f $logfile ]; then
-  touch $logfile
-fi 
-
-exec 2>> $logfile
-
-echo "Script started.."
-
-ls /abt
-
-echo "script ended at $(date)"
