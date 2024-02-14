b2369abea21a9745ab0a8a853651fdd8d2bbe2e7
abishekanand <abishekanand15@gmail.com>
2024-02-14 13:58:57 +0530

    Useradd Script edited

--- CreateUser.sh
+++ CreateUser.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-read -p "Enter username : " name
-# -s is silent. i.e., to enter the pass without displaying 
-read -s -p "Enter password : " pass
-read -p "Enter Home dir : " homedir
 
-if [ -z $homedir ]; then
-  homedir="/home/$name"
-fi  
+# Prompt the administrator for the username
+read -p "Enter the username for the new user: " username
 
-# -s is to set the shell for the user
-sudo useradd -m -d $homedir -s /bin/bash $name
+# Prompt the administrator for the home directory
+read -p "Enter the home directory for the new user (press Enter for default): " home_dir
 
+# If the home directory is not provided, set it to the default
+if [ -z "$home_dir" ]; then
+    home_dir="/home/$username"
+fi
+
+# Prompt the administrator for the password
+read -s -p "Enter the password for the new user: " password
+echo
+
+# Create the user account with the provided information
+sudo useradd -m -d "$home_dir" -s /bin/bash "$username"
+
+# Set the password for the new user
 echo "$username:$password" | sudo chpasswd
 
+# Check if the user account creation was successful
 if [ $? -eq 0 ]; then
     echo "User account '$username' created successfully."
 else
